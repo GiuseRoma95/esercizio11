57903b0c10cdbeecf6b6570ee7d39f26d54589c4
GiuseRoma95 <112175641+GiuseRoma95@users.noreply.github.com>
2022-09-12 16:58:53 +0200

    esercitazioni varie

---
+++ js/index.js
@@ -0,0 +1,2 @@
+alert("Pensa ad un numero. \n Il tuo risultato sarà 5");
+//window.alert('Sono ancora qua');
---
+++ js/naming.js
@@ -0,0 +1,39 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+/*var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);*/
+
+var primoNumero = Number(prompt("Scrivi il tuo numero pensato+10"));
+var secondoNumero = Number(prompt("Togli 4 (SCRIVI 4)"));
+var terzoNumero = Number(prompt("Sottrai il numero pensato"));
+var quartoNumero = Number(prompt("Sottrai 1"));
+
+document.getElementById("risultato").innerHTML =
+  primoNumero - secondoNumero - terzoNumero - quartoNumero;
+
+console.log("Risultato 5");
+
+//var 12 = 'numero';
+
+/*let sign = prompt("What's your sign?");
+
+if (sign.toLowerCase() === "scorpio") {
+  alert("Wow! I'm a Scorpio too!");
+}*/
+
+/* there are many ways to use the prompt feature
+sign = window.prompt(); // open the blank prompt window
+sign = prompt();       //  open the blank prompt window
+sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
+sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
+*/
